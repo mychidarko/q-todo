15c906c84fe292c6ab8e47576bc4f25d2d29c611
mr-phlames <mickdd22@gmail.com>
2020-06-13 23:29:44 +0000

    seperated config and storage && added authentication redirects

--- vue-app-template/src/router/index.js
+++ vue-app-template/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import { routes } from "../app/index";
+import { routes } from "@/app/index";
+import Token from "@/storage/token.storage";
 
 Vue.use(Router);
 
@@ -10,6 +11,20 @@ const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/auth/login", "/auth/register"];
+  const authRequired = !publicPages.includes(to.path);
+  const hasAuth = Token.get();
+
+  if (authRequired && !hasAuth) {
+    return next("/auth/login");
+  } else if (!authRequired && hasAuth) {
+    return next("/home");
+  }
+
+  next();
 });
 
 export default router;
