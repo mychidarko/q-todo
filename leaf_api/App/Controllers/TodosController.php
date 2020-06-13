b1d9cad515f4d66b54c8c5aaaf7f62656842f8e7
mr-phlames <mickdd22@gmail.com>
2020-06-13 21:51:50 +0000

    swapped TodosController for resource controller

--- leaf_api/App/Controllers/TodosController.php
+++ leaf_api/App/Controllers/TodosController.php
@@ -1,11 +1,53 @@
 <?php
-
 namespace App\Controllers;
 
-class TodosController extends Controller {
+class TodosController extends Controller {    
+    /**
+     * Display a listing of the resource.
+     */
     public function index() {
-        $this->respond([
-            "message" => "This is the TodosController"
-        ]);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store() {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update($id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id) {
+        //
     }
 }
