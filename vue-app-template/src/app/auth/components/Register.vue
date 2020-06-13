78c70e0edf4398957aed01e7cb13ed04f75f2749
mr-phlames <mickdd22@gmail.com>
2020-06-13 23:35:57 +0000

    added register component & route

---
+++ vue-app-template/src/app/auth/components/Register.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="register">Register</div>
+</template>
+
+<script>
+export default {
+  name: "Register"
+};
+</script>
--- vue-app-template/src/app/auth/routes/index.js
+++ vue-app-template/src/app/auth/routes/index.js
@@ -1,10 +1,17 @@
 import { Login } from "../components";
+import { Register } from "../components";
 
 export default [
   {
-    path: "/login",
+    path: "/auth/login",
     component: Login,
     name: "login",
+    meta: {}
+  },
+  {
+    path: "/auth/register",
+    component: Register,
+    name: "register",
     meta: {}
   }
 ];
