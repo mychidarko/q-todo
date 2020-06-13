e8b09e46e2914df13e0ad306a42a491cddbf0ac6
mr-phlames <mickdd22@gmail.com>
2020-06-13 23:17:13 +0000

    added config files

---
+++ vue-app-template/src/config/index.js
@@ -0,0 +1,5 @@
+export const APP_URL = window.location.origin;
+export const API_URL = "http://localhost:5500";
+export const USER_STORAGE_KEY = "user";
+export const TOKEN_STORAGE_KEY = "token";
+export const MAINTAINANCE_MODE = false;
---
+++ vue-app-template/src/config/user.storage.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { API_URL } from '@/common/config';
+import { USER_STORAGE_KEY } from './config';
+
+export function get(selector = null) {
+	return selector ? JSON.parse(window.localStorage.getItem(USER_STORAGE_KEY))[selector] : JSON.parse(window.localStorage.getItem(USER_STORAGE_KEY));
+}
+
+export function save(user) {
+	window.localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+}
+
+export function remove() {
+	window.localStorage.removeItem(USER_STORAGE_KEY);
+}
+
+export function refreshToken($function = null) {
+	return axios
+		.get(`${API_URL}/auth/refresh/${get("id")}`)
+		.then((res) => {
+			let user = get();
+			user.token = res.data.token;
+			save(user);
+
+			if ($function !== null) return $function();
+		});
+}
+
+export default { get, save, remove, refreshToken };
