1648b62ffdbe60996e4b62d527eed479c992eaa1
mr-phlames <mickdd22@gmail.com>
2020-06-13 21:20:30 +0000

    added leaf api package && updated readme

---
+++ leaf_api/App/Routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Set up 404 handler
+|--------------------------------------------------------------------------
+|
+| Create a handler for 404 errors
+|
+*/
+$app->set404(function () use ($app) {
+	$app->response->respondWithCode([
+		"data" => "Resource not found",
+	], 404);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Set up Controller namespace
+|--------------------------------------------------------------------------
+|
+| This allows you to directly use controller names instead of typing
+| the controller namespace first.
+|
+*/
+$app->setNamespace("\App\Controllers");
+
+$app->get("/", function() use($app) {
+	$app->response->respondWithCode("Congrats!! You're on Leaf API", 200);
+});
