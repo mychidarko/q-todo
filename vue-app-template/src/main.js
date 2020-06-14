91f1ac4e85b5c35bf6c8518b6fdd8277d28dff36
mr-phlames <mickdd22@gmail.com>
2020-06-14 12:32:25 +0000

    installed axios, bootstrap and bootstrap-vue

--- vue-app-template/src/main.js
+++ vue-app-template/src/main.js
@@ -2,8 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue, IconsPlugin, CardPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(CardPlugin);
 
 new Vue({
   router,
